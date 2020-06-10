3fcaa2bd89170092af65f5df451e814b1315c5c0
HenryKasai <henrykasai1775@gmail.com>
2020-06-10 18:07:40 -0300

    Exercício Resolvido.

--- Program.cs
+++ Program.cs
@@ -6,46 +6,52 @@ namespace Media4Notas
     {
         static void Main(string[] args)
         {   
+            decimal nota1, nota2, nota3, nota4;
+            
+            
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Insira as 4 notas que serão avaliadas.");
             Console.Write("Nota 1: ");
-            decimal  nota1 = Convert.ToDecimal(Console.ReadLine());
+            bool nota1bool = Decimal.TryParse(Console.ReadLine(), out nota1);
             Console.Write("Nota 2: ");
-            decimal  nota2 = Convert.ToDecimal(Console.ReadLine());
+            bool nota2bool = Decimal.TryParse(Console.ReadLine(), out nota2);
             Console.Write("Nota 3: ");
-            decimal  nota3 = Convert.ToDecimal(Console.ReadLine());
+            bool nota3bool = Decimal.TryParse(Console.ReadLine(), out nota3);
             Console.Write("Nota 4: ");
-            decimal  nota4 = Convert.ToDecimal(Console.ReadLine());
+            bool nota4bool = Decimal.TryParse(Console.ReadLine(), out nota4);
             Console.Clear();
-            if(nota1 <= 10 && nota2 <= 10 && nota3 <= 10 && nota4 <= 10 && nota1 >= 0 && nota2 >= 0 && nota3 >= 0 && nota4 >= 0)
+            if (!(nota1bool && nota2bool && nota3bool && nota4bool) 
+            || nota1 > 10 || nota2 > 10 || nota3 > 10 || nota4 > 10 
+            || nota1 < 0 || nota2 < 0 || nota3 < 0 || nota4 < 0)
             {
-                Console.ForegroundColor = ConsoleColor.Green;
-                decimal resultado = (nota1 + nota2 + nota3 + nota4) / 4;
-                Console.WriteLine($"Nota 1: {nota1:N1}"); 
-                Console.WriteLine($"Nota 2: {nota2:N1}");                  
-                Console.WriteLine($"Nota 3: {nota3:N1}");                  
-                Console.WriteLine($"Nota 4: {nota4:N1}");  
-
-                if (resultado < 5)
-                {                   
-                    Console.WriteLine($"Você ficou com média {resultado:N1}. Reprovado.");
-                }
-                else if (resultado >= 5 && resultado <= 6)
-                {
-                    Console.WriteLine($"Você ficou com média {resultado:N1}. Em recuperação.");
-                }
-                else if (resultado > 6)
-                {
-                    Console.WriteLine($"Você ficou com média {resultado:N1}. Aprovado.");
-                }
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Valor inválido.");
+                Console.ForegroundColor = ConsoleColor.Gray;    
+                Environment.Exit(-1);    
             }
             else
             {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Digite somente notas entre 0 e 10.");
-            }
-            Console.ResetColor();
-            Console.ReadKey();
+                decimal resultado = (nota1 + nota2 + nota3 + nota4) / 4;
+                Console.ForegroundColor = ConsoleColor.Green;
+                if (resultado < 5)
+                {
+                    Console.WriteLine($"Você ficou com média {resultado}, reprovado.");
+                }
+                else if (resultado >= 5 && resultado <= 6)
+                {
+                    Console.WriteLine($"Você ficou com média {resultado}, em recuperação.");
+                }
+                else if (resultado > 6)
+                {
+                    Console.WriteLine($"Você ficou com média {resultado}, aprovado.");
+                }
+            }   
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.ReadKey();    
+ 
+            
+
+
 
 
 
